af2a994016430a52fb1fab0c085592d38af2a46c
chrish1 <c.hoedl@gmx.net>
2012-07-24 21:58:00 +0200

    Until exercise 2.6.2

--- chapter2.scala
+++ chapter2.scala
@@ -1,8 +1,9 @@
 object chapter2 {
-
   val abs: Int => Int = n => if (n < 0) -n else n
 
   type Pred[A] = A => Boolean
+  
+  type BindF[A] = (A, A) => A
 
   def isDivisibleBy(k: Int): Pred[Int] = n => n % k == 0
 
@@ -11,5 +12,12 @@ object chapter2 {
   val isOdd: Pred[Int] = not(isEven)
 
   def not(p: Pred[Int]): Pred[Int] = n => !(p(n))
+
+  def isDivisibleBy3And5: Pred[Int] = n => lift(_ && _)(n)
   
+  def isDivisibleBy3Or5: Pred[Int] = n => lift(_ || _)(n)
+
+  // Not really what is wanted in 2.6.2?
+  def lift(bindF: BindF[Boolean]): Pred[Int] = n => 
+    bindF(isDivisibleBy(3)(n), isDivisibleBy(5)(n))
 }
