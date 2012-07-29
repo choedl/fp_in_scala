bea999616939ab68a8d467f4b171980469137ecd
chrish1 <c.hoedl@gmx.net>
2012-07-29 18:54:55 +0200

    Until exercise 2.6.3

--- chapter2.scala
+++ chapter2.scala
@@ -17,7 +17,15 @@ object chapter2 {
   
   def isDivisibleBy3Or5: Pred[Int] = n => lift(_ || _)(n)
 
-  // Not really what is wanted in 2.6.2?
   def lift(bindF: BindF[Boolean]): Pred[Int] = n => 
+//    bind(n, bindF, 
     bindF(isDivisibleBy(3)(n), isDivisibleBy(5)(n))
+
+  def applyIf[A](a: A, p: A => Boolean, f: A => A): A = {
+    // TODO better names
+    val bindF: (Boolean, A) => A = (pp, aa) => if (pp) f(aa) else aa
+    bind(a, p, bindF)
+  }
+
+  def bind[A, B, C](a: A, p: A => B, f: (B, A) => C): C = f(p(a), a)
 }
