c5df8305457482140a466504c2af71768b28bc0c
chrish1 <c.hoedl@gmx.net>
2012-08-03 17:22:51 +0200

    Until exercise 2.7.2

--- chapter2.scala
+++ chapter2.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object chapter2 {
   val abs: Int => Int = n => if (n < 0) -n else n
 
@@ -18,7 +20,7 @@ object chapter2 {
   def isDivisibleBy3Or5: Pred[Int] = n => lift(_ || _)(n)
 
   def lift(bindF: BindF[Boolean]): Pred[Int] = n => 
-//    bind(n, bindF, 
+    // TODO exercise 2.6.4 missing
     bindF(isDivisibleBy(3)(n), isDivisibleBy(5)(n))
 
   def applyIf[A](a: A, p: A => Boolean, f: A => A): A = {
@@ -28,4 +30,29 @@ object chapter2 {
   }
 
   def bind[A, B, C](a: A, p: A => B, f: (B, A) => C): C = f(p(a), a)
+ 
+  def fib(n: Int): Int = n match {
+    case 1 | 2 => 1
+    case _ => fib(n - 1) + fib(n - 2)
+  }
+
+  def fib1(n: Int): Int = {
+    @tailrec def fib1Tr(n: Int, b: Int, a: Int): Int = n match {
+      case 0 => a
+      case _ => fib1Tr(n - 1, a + b, b)
+    }
+    fib1Tr(n, 1, 0)
+  }
+  
+  def sqrt(n: Double): Double = {
+    def f(x: Double) = (x * x) - n
+    iterateWhile(2.0)(x => x - f(x) / (2 * x),
+                      x => f(x).abs > 1e-14)
+  }
+
+  @tailrec
+  def iterateWhile[A](a: A)(f: A => A, p: Pred[A]): A = p(a) match {
+    case true => iterateWhile(f(a))(f, p)
+    case false => f(a)
+  }
 }
